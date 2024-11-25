7f3fd00bf609a5335916b90ae06d21e7a4985f86
emirhanselcuk <selcukemirhan88@gmail.com>
2024-11-25 17:34:06 +0300

    Hata mesajlarının türkçeleştirilmesi Error Descrriber

---
+++ EmirhanIdentityApp.Web/Localizations/LocalizationIdentityErrorDescriber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmirhanIdentityApp.Web.Localizations
+{
+    public class LocalizationIdentityErrorDescriber:IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new() { Code = "DuplicateUserName", Description = $"{userName} daha önce başka bir kullanıcı tarafından alınmıştır." };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new() { Code = "DuplicateEmail", Description = $"{email}  adresi daha önce başka bir kullanıcı tarafından alınmıştır." };
+
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new() { Code = "PasswordTooShort", Description = $"Şifre en az 6 karakterli olmalıdır." };
+
+        }
+    }
+}
