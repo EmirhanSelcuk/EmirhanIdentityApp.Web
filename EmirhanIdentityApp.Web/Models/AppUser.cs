a67755d1ed5e2b98af3658e34473c98a299a6fa1
emirhanselcuk <selcukemirhan88@gmail.com>
2024-11-25 10:47:32 +0300

    Add project files.

---
+++ EmirhanIdentityApp.Web/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmirhanIdentityApp.Web.Models
+{
+    public class AppUser:IdentityUser
+    {
+    }
+}
---
+++ EmirhanIdentityApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmirhanIdentityApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
